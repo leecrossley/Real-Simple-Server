d3dd5e78cbfc2919de5763ba15c1fc95c66200b0
Lee Crossley <leee@hotmail.co.uk>
2012-05-30 15:45:19 +0100

    basic implementation

---
+++ main.js
@@ -0,0 +1,45 @@
+/*global require */
+
+var http = require("http");
+var fs = require("fs");
+var path = require("path");
+
+function spinUp(port) {
+	"use strict";
+	if (typeof (port) === "undefined") {
+		port = 8125;
+	}
+	http.createServer(function (request, response) {
+		if (request.url === "/") {
+			request.url = "/index.html";
+		}
+		var filePath = "." + request.url,
+			fileExt = path.extname(filePath),
+			contentType;
+
+		if (fileExt === ".js") {
+			contentType = "text/javascript";
+		} else if (fileExt === ".css") {
+			contentType = "text/css";
+		} else {
+			contentType = "text/html";
+		}
+
+		path.exists(filePath, function (exists) {
+			if (exists) {
+				fs.readFile(filePath, function (error, content) {
+					if (error) {
+						response.writeHead(500);
+						response.end();
+					} else {
+						response.writeHead(200, { "Content-Type": contentType });
+						response.end(content, "utf-8");
+					}
+				});
+			} else {
+				response.writeHead(404);
+				response.end();
+			}
+		});
+	}).listen(port);
+}
